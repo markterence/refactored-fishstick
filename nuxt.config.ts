b268bf3cb598e02b5abb28ae1d13227d0c822e39
Mark Terence Tiglao <github@markterence.me>
2025-05-05 15:36:58 +0800

    fix: update build command and base URL in configuration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  ssr: false,
+  app: {
+    baseURL: '/refactored-fishstick/'
+  },
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
 
